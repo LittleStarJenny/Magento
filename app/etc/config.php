15ebda978397af5b6db99f94dba1bab7010ef79d
Jenny Norén <littlestarjenny6@gmail.com>
2019-09-05 13:15:30 +0200

    added module

--- app/etc/config.php
+++ app/etc/config.php
@@ -212,6 +212,9 @@ return [
         'Klarna_Kp' => 1,
         'Magento_PaypalReCaptcha' => 1,
         'MSP_TwoFactorAuth' => 1,
+        'Mageplaza_Core' => 1,
+        'Mageplaza_Blog' => 1,
+        'Mageplaza_Gdpr' => 1,
         'Temando_Shipping' => 1,
         'Vertex_Tax' => 1
     ]
